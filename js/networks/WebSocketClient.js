1bb16239539debdcb210b28d8ca2e5050b5f4cb6
Takahiro <hogehoge@gachapin.jp>
2017-06-03 12:20:05 +0900

    Write comment for WebSocketClient

--- js/networks/WebSocketClient.js
+++ js/networks/WebSocketClient.js
@@ -1,5 +1,17 @@
+/**
+ * @author Takahiro https://github.com/takahirox
+ *
+ * TODO
+ *   implement
+ */
+
 ( function () {
 
+	/**
+	 * WebSocketClient constructor.
+	 * General WebSocket based NetworkClient.
+	 * @param params - parameters for instantiate.
+	 */
 	THREE.WebSocketClient = function ( params ) {
 
 		THREE.NetworkClient.call( this, params );
