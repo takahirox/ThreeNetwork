f1b8d5f375952d204228af429403965badea695f
Takahiro <hogehoge@gachapin.jp>
2017-06-10 12:32:50 +0900

    Replace ICE_SERVERS property name from url to urls in WebRTCClient

--- js/networks/WebRTCClient.js
+++ js/networks/WebRTCClient.js
@@ -195,11 +195,11 @@
 	// ice servers for RTCPeerConnection.
 
 	var ICE_SERVERS = [
-		{ url: 'stun:stun.l.google.com:19302' },
-		{ url: 'stun:stun1.l.google.com:19302' },
-		{ url: 'stun:stun2.l.google.com:19302' },
-		{ url: 'stun:stun3.l.google.com:19302' },
-		{ url: 'stun:stun4.l.google.com:19302' }
+		{ urls: 'stun:stun.l.google.com:19302' },
+		{ urls: 'stun:stun1.l.google.com:19302' },
+		{ urls: 'stun:stun2.l.google.com:19302' },
+		{ urls: 'stun:stun3.l.google.com:19302' },
+		{ urls: 'stun:stun4.l.google.com:19302' }
 	];
 
 	/**
